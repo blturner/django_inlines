3303a40c4721dddfaf4200440debcc17b06cf6ab
Ben Spaulding <ben@projectevolution.com>
2013-06-21 13:36:22 -0600

    Make the django admin jQuery available to our js.

--- django_inlines/static/django_inlines/inlines.js
+++ django_inlines/static/django_inlines/inlines.js
@@ -1,27 +1,27 @@
 var DjangoInlines = DjangoInlines || {}
 
 
-$(function() {
-  
+django.jQuery(function($) {
+
   $('.vInlineTextArea').each(function(){
     var id = this.id;
     var div = $('<div id="inline_control_for_'+id+'" class="inline_control"><p class="insert">Insert inline: </p></div>');
     var select = $('<select><option value="">----</option></select>');
     for (inline in DjangoInlines.inlines) { select.append($('<option value='+DjangoInlines.inlines[inline]+'>'+DjangoInlines.inlines[inline]+'</option>'))};
     div.append($('<div class="inlineinserter" id="'+id+'_inlineinserter"></div>'))
-    
+
     select.change(function(){
       var inline = $(this).val();
       var inserter = $('#'+id+'_inlineinserter');
       if (inline == '') { inserter.html(''); return false }
       inserter.load(DjangoInlines.get_inline_form_url + "?inline="+$(this).val()+"&target="+id);
     });
-    
+
     div.find('p').append(select);
     $(this).after(div);
   });
 
-  
+
   $("div.inlineinserter .insert").live("click", function(){
     target = $(this).attr('rel');
     parent = $(this).parents('div.inline_control');
@@ -45,5 +45,5 @@ $(function() {
     $(this).parents('div.inline_control').find('p.insert select').val(['']).change();
     return false;
   });
-  
+
 });
