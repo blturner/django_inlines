011cb2d28b3ac7a9917ba4a63a1abb64cce184fc
Benjamin Turner <benturn@gmail.com>
2025-06-19 21:49:13 -0700

    Update admin_urls.py

--- django_inlines/admin_urls.py
+++ django_inlines/admin_urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import url
+from django.urls import re_path as url
 
 from . import views
 
 
 urlpatterns = [
-    url(r'^inline_config\.js$', views.js_inline_config, name='js_inline_config'),
-    url(r'^get_inline_form/$', views.get_inline_form, name='get_inline_form'),
+    url(
+        r"^inline_config\.js$", views.js_inline_config, name="js_inline_config"
+    ),
+    url(r"^get_inline_form/$", views.get_inline_form, name="get_inline_form"),
 ]
